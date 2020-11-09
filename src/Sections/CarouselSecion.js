397ad1845c8e6b44fedef01f0a67bb7c37c1bc39
amit1884 <amitraj200raj@gmail.com>
2020-11-09 11:00:55 +0530

    carousel changed and navbar added

--- src/Sections/CarouselSecion.js
+++ src/Sections/CarouselSecion.js
@@ -1,35 +1,44 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import img1 from '../assets/images/1.jpg'
 import img2 from '../assets/images/2.jpg'
 import img3 from '../assets/images/3.jpg'
 import img4 from '../assets/images/4.jpg'
 import '../assets/style.css'
+import {Carousel} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function CarouselSection() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay:true
-  };
   return (
-    <Slider {...settings}>
-        <div className="banner_container">
-        <img src ={img1} alt="img1"/>
-        </div>
-        <div className="banner_container">
-        <img src ={img2} alt="img1"/>
-        </div>
-        <div className="banner_container">
-        <img src ={img3} alt="img1"/>
-        </div>
-        <div className="banner_container">
-        <img src ={img4} alt="img1"/>
-        </div>
-    </Slider>
+   <React.Fragment>
+        <Carousel>
+        <Carousel.Item interval={1000}>
+            <img
+            className="d-block w-100 banner"
+            src={img1}
+            alt="First"
+            />
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+            <img
+            className="d-block w-100 banner"
+            src={img2}
+            alt="Third "
+            />
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+            <img
+            className="d-block w-100 banner"
+            src={img3}
+            alt="Third"
+            />
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+            <img
+            className="d-block w-100 banner"
+            src={img4}
+            alt="Fourth"
+            />
+        </Carousel.Item>
+        </Carousel>
+   </React.Fragment>
   );
 }
--- src/Sections/Navbar.js
+++ src/Sections/Navbar.js
@@ -1,11 +1,26 @@
 import React from 'react'
-
-function Navbar() {
+import logo from '../assets/images/logo.png'
+import {Navbar,Form,FormControl,Button} from 'react-bootstrap'
+function NavbarSection() {
     return (
-        <div>
-            Scholarship
-        </div>
+        <React.Fragment>
+      <Navbar bg="light" variant="dark" style={{borderBottom:'1px solid black'}}>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="200"
+            height="50"
+            className="d-inline-block align-top"
+          />{' '}
+        </Navbar.Brand>
+        <Form inline className="ml-auto">
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar>
+    </React.Fragment>
     )
 }
 
-export default Navbar
+export default NavbarSection
