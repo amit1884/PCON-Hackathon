8153416abdfe0467507471e9065d41ad77fd7c74
amit1884 <amitraj200raj@gmail.com>
2020-11-10 12:10:30 +0530

    server folder separated

--- server/constants.js
+++
@@ -1,1 +0,0 @@
-exports.MONGO_URL = `mongodb+srv://amit:raj@cluster0.hny5q.mongodb.net/scholarship?retryWrites=true&w=majority`
--- server/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const app = require('./index')
-
-const port = process.env.PORT || 8000;
-
-const server = http.createServer(app);
-
-// eslint-disable-next-line no-console
-server.listen(port, () => console.log(`listening on PORT ${port}`));
